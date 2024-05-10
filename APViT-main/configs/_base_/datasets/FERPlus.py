0b23909c9344cb9cc36e09ce30e8dc9b10430f40
TinvalInacmar <vm52396@fer.hr>
2024-05-10 18:01:52 +0200

    a

--- APViT-main/configs/_base_/datasets/FERPlus.py
+++ APViT-main/configs/_base_/datasets/FERPlus.py
@@ -39,7 +39,7 @@ test_pipeline = [
     dict(type='Collect', keys=['img', ])
 ]
 
-base_path = 'kaggle/input/ferplus/FERPlus/'
+base_path = '../../../input/ferplus/FERPlus/'
 image_path = base_path + 'Image/'    # we use realigned images
 
 data = dict(
